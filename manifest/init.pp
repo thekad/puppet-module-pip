d9c0ae07d0a2b9a56baeace76165bbc9c8f6763c
Jorge A Gallegos <kad@blegh.net>
2011-09-17 12:43:57 -0700

    Adding modelines

--- manifest/init.pp
+++ manifest/init.pp
@@ -1,3 +1,6 @@
+# -*- mode: puppet; sh-basic-offset: 4; indent-tabs-mode: nil; coding: utf-8 -*-
+# vim: tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8
+
 class library {
 
     $pip_program = $operatingsystem ? {
